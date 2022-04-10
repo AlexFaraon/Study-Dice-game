11ebbc25e46e9b7aea23b52f31184d364ffd606f
AlexFaraon <103384514+AlexFaraon@users.noreply.github.com>
2022-04-10 22:34:44 +0300

    Java first step
    my first step in java (HW)

---
+++ CompareNumbers.java
@@ -0,0 +1,20 @@
+package ee.homeproject.first;
+/*
+Создайте метод compareNumbers(), в теле которого объявите две int переменные a и b,
+и инициализируйте их любыми значениями, которыми захотите.
+ Если a больше или равно b, то необходимо вывести в консоль сообщение “a >= b”, в противном случае “a < b”;
+ */
+public class CompareNumbers {
+    public static void main(String [] args){
+        int a = 9;
+        int b = 8;
+
+        if (a >= b) {
+            System.out.println("a >= b");
+        }
+        else  {
+            System.out.println("a < b");
+        }
+
+    }
+}
---
+++ FirstApp.java
@@ -0,0 +1,29 @@
+package ee.homeproject.first;
+
+/*Создайте метод printThreeWords(), который при вызове должен отпечатать в столбец три слова: Orange, Banana, Apple.
+Orange
+Banana
+Apple
+ */
+
+public class FirstApp {
+        // Aleksei Faraon
+        // 2.Создайте метод printThreeWords(), который при вызове должен отпечатать в столбец три слова: Orange, Banana, Apple.
+    public static void main(String [] args){
+        System.out.println("Hello, this is my first step in Java...");
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
