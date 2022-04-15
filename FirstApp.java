8308baeefc8c77615ac6aa23ecde14317c1460ef
AlexFaraon <103384514+AlexFaraon@users.noreply.github.com>
2022-04-15 18:45:01 +0300

    Update FirstApp.java

--- FirstApp.java
+++ FirstApp.java
@@ -9,7 +9,7 @@ Apple
 public class FirstApp {
         // Aleksei Faraon
         // 2.Создайте метод printThreeWords(), который при вызове должен отпечатать в столбец три слова: Orange, Banana, Apple.
-    public static void main(String [] args){
+    public static void printThreeWords(String [] args){
         System.out.println("Hello, this is my first step in Java...");
         System.out.println("Orange");
         System.out.println("Banana");
