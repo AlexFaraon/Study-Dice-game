e9b04b13835df68663f5fac941eb7d792d904ec9
AlexFaraon <103384514+AlexFaraon@users.noreply.github.com>
2022-04-15 19:57:21 +0300

    Update FirstApp.java

--- FirstApp.java
+++ FirstApp.java
@@ -1,50 +1,92 @@
 package ee.homeproject.first;
 
-public class Lesson2 {
-    public static void main(String[] args) {
+/*Создайте метод printThreeWords(), который при вызове должен отпечатать в столбец три слова: Orange, Banana, Apple.
+Orange
+Banana
+Apple
+ */
 
-    }
-    // 1. Написать метод, принимающий на вход два целых числа и проверяющий,
-// что их сумма лежит в пределах от 10 до 20 (включительно),
-// если да – вернуть true, в противном случае – false.
-    public static boolean  methodOne(int a, int b){
-        int sum = a + b;
-        if ((a+b <= 20) && (a+b >= 10)) {
-            return true;
-        } else {
-            return false;
+public class FirstApp {
+        // Aleksei Faraon
+        // 1.Создайте метод printThreeWords(), который при вызове должен отпечатать в столбец три слова: Orange, Banana, Apple.
+    public static void main(String [] args){
+        System.out.println("Hello, this is my first step in Java...");
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
         }
     }
 
-//2. Написать метод, которому в качестве параметра передается целое число,
-// метод должен напечатать в консоль, положительное ли число передали или отрицательное.
-// Замечание: ноль считаем положительным числом.
-public static void NegOrPosNumber(int num) {
-    if (num >= 0) {
-        System.out.println("Positive");
-    } else {
-        System.out.println("Negative");
+
+/*
+2.Создайте метод checkSumSign(), в теле которого объявите две int переменные a и b, и инициализируйте их
+любыми значениями, которыми захотите. Далее метод должен просуммировать эти переменные,
+и если их сумма больше или равна 0, то вывести в консоль сообщение “Сумма положительная”,
+в противном случае - “Сумма отрицательная”;
+*/
+class SumSignCheck {
+    private static void main(String [] args){
+        int a = 7;
+        int b = 8;
+        int c = a + b;
+
+        if (c >= 0) {
+            System.out.println("Сумма положительная!");
+        }
+        else  {
+            System.out.println("Сумма отрицательная!");
+        }
+
+    }
+}
+
+/*
+3. Создайте метод printColor() в теле которого задайте int переменную value и инициализируйте ее любым значением.
+Если value меньше 0 (0 включительно), то в консоль метод должен вывести сообщение “Красный”,
+если лежит в пределах от 0 (0 исключительно) до 100 (100 включительно),то “Желтый”,
+если больше 100 (100 исключительно) - “Зеленый”;
+ */
+class ColorPrint {
+    public static void printColor(String[] args) {
+        int znach = 77;
+        if (znach <= 0) {
+            System.out.println("RED");
+        }
+        if (znach > 1 && znach < 100) {
+            System.out.println("YELLOW");
+        }
+        if (znach > 101) {
+            System.out.println("GREEN");
+        }
+        System.out.println("GoodJob");
     }
 }
 
 
-//3. Написать метод, которому в качестве параметра передается целое число.
-//Метод должен вернуть true, если число отрицательное, и вернуть false если положительное.
-public static boolean TrueOrFalse(int sum){
-        if (sum >= 0) {
-            return true;
-        } else {
-            return false;
+/*
+4.Создайте метод compareNumbers(), в теле которого объявите две int переменные a и b,
+и инициализируйте их любыми значениями, которыми захотите.
+ Если a больше или равно b, то необходимо вывести в консоль сообщение “a >= b”, в противном случае “a < b”;
+ */
+class CompareNumbers {
+    public static void compareNumbers(String [] args){
+        int a = 9;
+        int b = 8;
+
+        if (a >= b) {
+            System.out.println("a >= b");
         }
+        else  {
+            System.out.println("a < b");
+        }
+
+    }
 }
 
-//4. Написать метод, которому в качестве аргументов передается строка и число,
-// метод должен отпечатать в консоль указанную строку, указанное количество раз
-public static void printStringNum(String word, int number) {
-        printStringNum("Hello", 5);
-}
 
-}
+
+
+
 
 
 
